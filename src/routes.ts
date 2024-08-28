f54133414a8b7b3f970ef30ffa2004a76dd627f0
Matheus Oliveira <matheus.oliveira@ccc.ufcg.edu.br>
2024-08-28 13:56:14 -0300

    fix: test

--- src/routes.ts
+++ src/routes.ts
@@ -5,14 +5,32 @@ import authMiddleware from './middlewares/auth';
 import ProjectController from './app/controllers/ProjectController';
 import TaskController from './app/controllers/TaskController';
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import AWS from 'aws-sdk';
 
 const router = Router();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_DEFAULT_REGION,
+});
+
 const upload = multer({
-    storage: multer.memoryStorage(),
-    limits: {
-      fileSize: 5 * 1024 * 1024, // limit file size to 5MB
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET_NAME,
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
     },
-  });
+    key: (req, file, cb) => {
+      cb(null, `uploads/${Date.now().toString()}_${file.originalname}`);
+    },
+  }),
+  limits: {
+    fileSize: 5 * 1024 * 1024, // limit file size to 5MB
+  },
+});
 
 router.get('/', (req, res) => {
     res.status(200).json({ status: 'ok' });
